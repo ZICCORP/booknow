7815471c03d503cdebeee5ba9e96cff2f62a4add
ZICCORP <frankchuka250@gmail.com>
2020-09-12 02:24:26 +0100

    worked on some utility tools

--- main/models.py
+++ main/models.py
@@ -5,6 +5,7 @@ class ActiveManager(models.Manager):
         return self.filter(active=True)
 
 class Product(models.Model):
+    tag = models.ManyToManyField('ProductTag', blank=True)
     name = models.CharField(max_length=32)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -13,6 +14,10 @@ class Product(models.Model):
     in_stock = models.BooleanField(default=True)
     date_updated = models.DateTimeField(auto_now=True)
     objects = ActiveManager()
+
+
+    def __str__(self):
+        return self.name
 
 
 class ProductImage(models.Model):
@@ -24,11 +29,24 @@ class ProductImage(models.Model):
         upload_to="product-thumbnails", null=True
     )
 
+    def __str__(self):
+        return self.product__name
+
+class ProductTagManager(models.Manager):
+    def get_by_natural_key(self,slug):
+        return self.get(slug=slug)
 
 class ProductTag(models.Model):
-    products = models.ManyToManyField(Product, blank=True)
+    
     name = models.CharField(max_length=32)
     slug = models.SlugField(max_length=48)
     description = models.TextField(blank=True)
     active = models.BooleanField(default=True)
+    objects = ProductTagManager()
+
+    def natural_key(self):
+        return (self.slug,)
+
+    def __str__(self):
+        return self.name
 
